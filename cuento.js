955c908e7a0da1a9192bfa9325ecf76156a8de86
RITA Y ROXANA <ritafabiola3010@hotmail.com>
2016-03-20 17:40:12 -0500

    cuento

---
+++ cuento.js
@@ -0,0 +1,16 @@
+var verbo = prompt("Hola Usuario, Ingrese un verbo!");
+var adjetivo = prompt("Hola Usuario, Ingrese un adjetivo!");
+var sust = prompt("Hola Usuario, Ingrese un sustantivo!");
+
+
+document.write("<h1>la familia " + verbo.toUpperCase());
+document.write(" paseaba  " + adjetivo.toUpperCase());
+document.write("  por sus vacaciones        " + sust);
+
+document.write("<h1> el gato " + verbo.toUpperCase());
+document.write(" ronroneaba " + adjetivo.toUpperCase());
+document.write("  por el tejado       " + sust);
+
+document.write(" <h1> la niña " + verbo.toUpperCase());
+document.write(" bonita" + adjetivo.toUpperCase());
+document.write("  se presenta en el coliseo       " + sust);
--- scripts.js
+++ scripts.js
@@ -1,3 +1,23 @@
+/*console.log("Inicio de programa");
 alert("Hola JavaScript");
 document.write("<h1>Hola JavaScript</h1>");
 alert("Gracias por visitarnos");
+*/
+
+console.log("Inicio de programa");
+alert("Hola JavaScript");																																																							
+document.write("<h1>Hola JavaScript!</h1>");
+alert("Gracias por visitar");
+console.log("Fin de programa");
+
+var equipo = prompt("Cual es tu equipo de futbol favorito?");
+console.log("El equipo es: " + equipo);
+
+/**var clave = prompt("Hola Usuario, Ingrese su clave!");
+console.log("La longitud de la clave es: " + clave.length);**/
+
+var clave = prompt("Hola Usuario, Ingrese su clave en minusculas!");
+console.log("La longitud de la clave es: " + clave.length);
+console.log("TA-DA!!! Su clave es: " + clave.toUpperCase());
+console.log("El valor original de la clave es: " +  clave);
+
