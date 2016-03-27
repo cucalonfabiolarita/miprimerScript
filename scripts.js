7c0039f71d34ae49074cc57ab828e71db933a960
RITA Y ROXANA <ritafabiola3010@hotmail.com>
2016-03-27 12:04:15 -0500

    ejercicio adivina el numero

--- scripts.js
+++ scripts.js
@@ -22,10 +22,46 @@ console.log("TA-DA!!! Su clave es: " + clave.toUpperCase());
 console.log("El valor original de la clave es: " +  clave);*/
 
 
-console.log("Inicio del PROGRAMA");
+/*console.log("Inicio del PROGRAMA");
 var equipo = prompt("Ingrese un numero");
 var dado= Math.floor(Math.random() *equipo);
 alert("Dado dice:"+ dado);
 console.log("Programa completado");
+*/
 
+document.write("<h1>ADIVINA EL NUMERO!</h1>");
+var adivinoCorrectamente=false;
+var numeroAdivinable= Math.floor(Math.random()*10)+1;
+var numeroUsuario= prompt("CUAL PIENSAS QUE ES EL NUMERO SECRETO(del 1 al 10)?");
+if(parseInt(numeroUsuario)===numeroAdivinable)
+{
+adivinoCorrectamente=true;
+} else if(parseInt(numeroUsuario)>numeroAdivinable)
+{ 
+alert("numero menor que el que pusiste!"); 
+}
+ var nuevoIntento = prompt("Intentalo de nuevo"); 
+if(parseInt(nuevoIntento)===numeroAdivinable) 
+{
+	adivinoCorrectamen=true;
+
+}
+else if(parseInt(numeroUsuario)<numeroAdivinable) 
+{ 
+alert("El numero es mayor al que dijiste!"); 
+var nuevoIntento = prompt("Intentalo de nuevo");
+}
+if(parseInt(nuevoIntento)===numeroAdivinable) 
+{
+adivinoCorrectamente=true;
+}
+if(adivinoCorrectamente)
+{
+	document.write("<p>ganastes</p>");
+}
+else {
+	document.write("<p>Perdistes. El numero correcto era" +numeroAdivinable+"</p>");
+}
+
+console.log("programa Terminado");
 
