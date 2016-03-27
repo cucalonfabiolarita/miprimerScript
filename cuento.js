8fd1248716dc63ddbbffa43dfb50acab695adde1
RITA Y ROXANA <ritafabiola3010@hotmail.com>
2016-03-27 10:49:07 -0500

    agegado archivo faltante a cuento

--- cuento.js
+++ cuento.js
@@ -1,16 +1,13 @@
-var verbo = prompt("Hola Usuario, Ingrese un verbo!");
-var adjetivo = prompt("Hola Usuario, Ingrese un adjetivo!");
-var sust = prompt("Hola Usuario, Ingrese un sustantivo!");
+var preguntas = 3;
+var preguntasFaltantes = "Quedan " + preguntas + " preguntas.";
+alert("Este es el juego del cuenta cuentos. Sigue las instrucciones.");
+var sustantivo1 = prompt("Ingrese un Sustantivo. " + preguntasFaltantes);
+preguntas -= 1;
+preguntasFaltantes = "Quedan " + preguntas + " preguntas.";
+var adjectivo1 = prompt("Ingrese un Adjectivo " + preguntasFaltantes);
+preguntas -= 1;
+preguntasFaltantes = "Quedan " + preguntas + " preguntas.";
+var verbo1 = prompt("Ingrese un verbo " + preguntasFaltantes);
 
-
-document.write("<h1>la familia " + verbo.toUpperCase());
-document.write(" paseaba  " + adjetivo.toUpperCase());
-document.write("  por sus vacaciones        " + sust);
-
-document.write("<h1> el gato " + verbo.toUpperCase());
-document.write(" ronroneaba " + adjetivo.toUpperCase());
-document.write("  por el tejado       " + sust);
-
-document.write(" <h1> la niña " + verbo.toUpperCase());
-document.write(" bonita" + adjetivo.toUpperCase());
-document.write("  se presenta en el coliseo       " + sust);
+var cuento = "El " + adjectivo1 + " " + sustantivo1 + " queria " + verbo1 + " en el balcon de su caleta!";
+document.write("<h1>Cuento</h1><p>" + cuento + "</p>");
